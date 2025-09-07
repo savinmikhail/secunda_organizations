2e586497b209bcf9fa2c912f0d3ca63c6361fcb9
Savin Mikhail <mikhail.d.savin@gmail.com>
2025-09-07 17:24:51 +0300

    fix pagination

--- app/routes/api.php
+++ app/routes/api.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\OrganizationController;
 Route::middleware('apikey')->group(function () {
     Route::get('/buildings/{building}/organizations', [OrganizationController::class, 'indexByBuilding'])
         ->name('buildings.organizations.index');
-});
 
+    // List organizations by activity (includes descendants)
+    Route::get('/activities/{activity}/organizations', [OrganizationController::class, 'indexByActivity'])
+        ->name('activities.organizations.index');
+});
